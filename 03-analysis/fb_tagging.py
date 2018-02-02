965e84baf6ad2a7a1d75f4000f5b5d6806c03b34
ralmsmeets <roger@squla.com>
2018-02-02 20:53:42 +0100

    add toptag functions

--- 03-analysis/fb_tagging.py
+++ 03-analysis/fb_tagging.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
+import os
 
-datapath = ''
+datapath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+
+fb_train = pd.read_csv(datapath+'/02-data/FB_Train.csv')
+
+def _extract_top_tags(df, tagvar, topn = 20):
+
+    tagcounts = df.groupby([tagvar])['Id'].nunique()
+
+    return tagcounts.sort_values(ascending=False).head(topn).keys()
+
+
+def _filter_df_on_top_tags(df, tagvar, toptaglist):
+
+    return df[df[tagvar].isin(toptaglist)]
+
 
