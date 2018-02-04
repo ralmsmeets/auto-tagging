ff8306122ca2008a3c29eca7d97b204f06ce6b23
ralmsmeets <roger@squla.com>
2018-02-04 22:37:48 +0100

    add remove_punctuation and remove_stopwords functions

--- 03-analysis/fb_tagging.py
+++ 03-analysis/fb_tagging.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import re
 
+from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from sklearn.metrics import confusion_matrix
 
@@ -18,8 +20,8 @@ from keras import utils
 
 
 datapath = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+so_tags = pd.read_csv(datapath+'/02-data/SO_Train.csv')
 
-so_train = pd.read_csv(datapath+'/02-data/SO_Train.csv')
 
 def _extract_top_tags(df, tagvar, topn = 20):
 
@@ -32,4 +34,26 @@ def _filter_df_on_top_tags(df, tagvar, toptaglist):
 
     return df[df[tagvar].isin(toptaglist)]
 
+
+def _remove_punctuation_from_string(givenstring):
+
+    return re.sub(r'[^\w\s\']','',givenstring)
+
+
+def _remove_stopwords(givenstring, language):
+
+    textlist = givenstring.lower().split()
+    thestopwords = stopwords.words(language)
+
+    cleantextlist = list(set(textlist) - set(thestopwords))
+
+    return ' '.join(cleantextlist)  
+
+
+def _create_train_test_data(df, trainproportion = 0.7):
+
+    toptaglist = _extract_top_tags(df, 'Tags')
+    filtereddf = _filter_df_on_top_tags(df, 'Tags', toptaglist)
+
+    filtereddf['cleanTitle'] = filtereddf['Title'].apply(_remove_stopwords,  args = ('english',))
 
